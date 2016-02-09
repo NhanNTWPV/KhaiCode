6aed16ba330dfeb67079d7b2906bce15f40f9b3a
nhanntwpv <hostdknhan2@gmail.com>
2016-02-09 17:39:51 +0700

    Khai code 2016

---
+++ KhaiCode2016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhaiCode2016
+{
+    /// <summary>
+    /// Khai code 2016 :)
+    /// @17h30 09/02/2016 - mung 2 tet
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy new year 2016 :)");
+            Console.ReadKey();
+        }
+    }
+}
